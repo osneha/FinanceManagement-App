5cff733a5c4d9a992481c0cd6e6e7fe473c069ad
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-11-11 14:49:32 -0600

    initial commit

---
+++ FinanceManagement-App/FinanceManagement-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceManagement-App
+//
+//  Created by Sneha Ojha on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
