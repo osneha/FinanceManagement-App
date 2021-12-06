76fe7ffd080ef0d5a7cd403b3bbcdc16f120c973
Drake Bush <dtbush18@gmail.com>
2021-12-05 19:06:46 -0600

    Log In Screen Complete
    add enable/disable feature for LogIn
    add placeholder change credentials

--- FinanceManagement-App/FinanceManagement-App/ViewController.swift
+++ FinanceManagement-App/FinanceManagement-App/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Log In View Controller Elements
+    
+    @IBOutlet weak var UsernameTextField: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    @IBOutlet weak var logInBTN: UIButton!
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    //Log In View Controller Variables
+   var username = ""
+   var password = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //View Setup
+        logInBTN.backgroundColor = UIColor.black
+        logInBTN.isEnabled = false
     }
-
+    
+    
+    @IBAction func onUsernameEdit(_ sender: Any) {
+        if (UsernameTextField.text == "")||(PasswordTextField.text == ""){
+            
+            logInBTN.backgroundColor = UIColor.black
+            logInBTN.isEnabled = false
+        }
+        else{
+            logInBTN.backgroundColor = UIColor.orange
+            logInBTN.isEnabled = true
+        }
+        
+    }
+    
+    
+    @IBAction func onPasswordEdit(_ sender: Any) {
+        if (UsernameTextField.text == "")||(PasswordTextField.text == ""){
+            
+            logInBTN.backgroundColor = UIColor.black
+            logInBTN.isEnabled = false
+        }
+        else{
+            logInBTN.backgroundColor = UIColor.orange
+            logInBTN.isEnabled = true
+        }
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func loginBtnAction(_ sender: Any) {
+    }
+    
+    @IBAction func ResetCredentialsBtnAction(_ sender: Any) {
+        
+    }
+    
 
 }
 
