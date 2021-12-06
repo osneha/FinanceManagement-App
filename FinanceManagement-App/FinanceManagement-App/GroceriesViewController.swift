e8e7bd05e3394f839dda6e279615ae185ef406cd
Ben Weddle <s530058@nwmissouri.edu>
2021-12-06 10:15:10 -0600

    Grocery View Done

--- FinanceManagement-App/FinanceManagement-App/GroceriesViewController.swift
+++ FinanceManagement-App/FinanceManagement-App/GroceriesViewController.swift
@@ -9,13 +9,41 @@ import UIKit
 
 class GroceriesViewController: UIViewController {
 
+    @IBOutlet weak var TotalAmt: UITextField!
+    
+    @IBOutlet weak var RemainingAmt: UITextField!
+    
+    @IBOutlet weak var ItemName: UITextField!
+    
+    @IBOutlet weak var ItemCost: UITextField!
+    
+    @IBOutlet weak var ItemNumber: UITextField!
+    
+    @IBOutlet weak var ItemTotal: UITextField!
+    
+    
+    
+    let tot = Budgets.shared.Groceries
+    var totSpent = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        TotalAmt.text = "\(tot)"
     }
     
-
+    @IBAction func OnClick(_ sender: Any) {
+        let x = Int(ItemCost.text!)
+        let y = Int(ItemNumber.text!)
+        let z = x! * y!
+        ItemTotal.text! = "\(z)"
+        totSpent += z
+        let rem = tot - totSpent
+        RemainingAmt.text! = "\(rem)"
+        
+    }
+    
     /*
     // MARK: - Navigation
 
