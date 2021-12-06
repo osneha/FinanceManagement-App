4e3dd255d466b1572b53143d8f98870bf7998851
Drake Bush <dtbush18@gmail.com>
2021-12-06 00:26:49 -0600

    Finished LogIn Page
    Added Reset Credentials Functionality
    Added Security Question
    Added Security Text Field
    Added SecurityQuestion Text
    Added Logic To all 3 Text Fields on edit

--- FinanceManagement-App/FinanceManagement-App/ViewController.swift
+++ FinanceManagement-App/FinanceManagement-App/ViewController.swift
@@ -10,12 +10,17 @@ class LogIn{
     static let Credentials = LogIn()
     var username :String = ""
     var password :String = ""
+    var answer :String = ""
 }
 class ViewController: UIViewController {
 //Log In View Controller Elements
     
     @IBOutlet weak var UsernameTextField: UITextField!
     
+    
+    @IBOutlet weak var FavoriteAnimalQuestion: UILabel!
+    @IBOutlet weak var SecurityQuestionTextField: UITextField!
+    @IBOutlet weak var SecurityQuestionLabel: UILabel!
     
     @IBOutlet weak var PasswordTextField: UITextField!
     
@@ -26,6 +31,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //View Setup
+        //self.navigationItem.hidesBackButton = true
         logInBTN.backgroundColor = UIColor.black
         logInBTN.isEnabled = false
     }
@@ -33,7 +39,10 @@ class ViewController: UIViewController {
     //Login Button Requirements---------------------
     @IBAction func onUsernameEdit(_ sender: Any) {
         if(LogIn.Credentials.username == "" && LogIn.Credentials.password == ""){
-            if (UsernameTextField.text == "")||(PasswordTextField.text == ""){
+            FavoriteAnimalQuestion.isHidden = false
+            SecurityQuestionLabel.isHidden = false
+            SecurityQuestionTextField.isHidden = false
+            if (UsernameTextField.text == "")||(PasswordTextField.text == "") || (SecurityQuestionTextField.text == ""){
             
                 logInBTN.backgroundColor = UIColor.black
                 logInBTN.isEnabled = false
@@ -69,7 +78,10 @@ class ViewController: UIViewController {
     }
     @IBAction func onPasswordEdit(_ sender: Any) {
         if(LogIn.Credentials.username == "" && LogIn.Credentials.password == ""){
-            if (UsernameTextField.text == "")||(PasswordTextField.text == ""){
+            FavoriteAnimalQuestion.isHidden = false
+            SecurityQuestionLabel.isHidden = false
+            SecurityQuestionTextField.isHidden = false
+            if (UsernameTextField.text == "")||(PasswordTextField.text == "") || (SecurityQuestionTextField.text == ""){
             
                 logInBTN.backgroundColor = UIColor.black
                 logInBTN.isEnabled = false
@@ -92,6 +104,50 @@ class ViewController: UIViewController {
 //---------------------------------------------------
     }//End of onPasswordEdit
     
+    
+   
+    @IBAction func onQuestionEdit(_ sender: Any) {
+        if(LogIn.Credentials.username == "" && LogIn.Credentials.password == ""){
+            FavoriteAnimalQuestion.isHidden = false
+            SecurityQuestionLabel.isHidden = false
+            SecurityQuestionTextField.isHidden = false
+            if (UsernameTextField.text == "")||(PasswordTextField.text == "") || (SecurityQuestionTextField.text == ""){
+            
+                logInBTN.backgroundColor = UIColor.black
+                logInBTN.isEnabled = false
+        }
+            else{
+                logInBTN.backgroundColor = UIColor.orange
+                logInBTN.isEnabled = true
+                }
+    } //end of outer If
+        if(SecurityQuestionTextField.text == LogIn.Credentials.answer){
+            LogIn.Credentials.username = ""
+            LogIn.Credentials.password = ""
+            LogIn.Credentials.answer = ""
+            SecurityQuestionTextField.text = ""
+            UsernameTextField.text = ""
+            PasswordTextField.text = ""
+            TextLabel.text = "Credentials Reset, Enter New Credentials"
+            
+        }
+        
+        /*
+        else{
+            if(UsernameTextField.text == LogIn.Credentials.username && PasswordTextField.text == LogIn.Credentials.password){
+                logInBTN.backgroundColor = UIColor.orange
+                logInBTN.isEnabled = true
+            }
+            else{
+                logInBTN.backgroundColor = UIColor.black
+                logInBTN.isEnabled = false
+            }*/
+        }
+   // }
+    
+    
+    
+    
     /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let transistion = segue.identifier
@@ -107,10 +163,19 @@ class ViewController: UIViewController {
     @IBAction func loginBtnAction(_ sender: Any) {
         LogIn.Credentials.username = UsernameTextField.text!
         LogIn.Credentials.password = PasswordTextField.text!
+        LogIn.Credentials.answer = SecurityQuestionTextField.text!
+      SecurityQuestionLabel.isHidden = true
+        SecurityQuestionTextField.isHidden = true
+        FavoriteAnimalQuestion.isHidden = true
     }
     
     @IBAction func ResetCredentialsBtnAction(_ sender: Any) {
+        UsernameTextField.text = ""
+        PasswordTextField.text = ""
         
+        FavoriteAnimalQuestion.isHidden = false
+        SecurityQuestionLabel.isHidden = false
+        SecurityQuestionTextField.isHidden = false
     }
     
 
