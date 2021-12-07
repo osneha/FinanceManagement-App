92c8b593c6428ceaa369cb38fc804a45db7fd86b
Noah Chase <42814547+s529296@users.noreply.github.com>
2021-12-07 03:14:40 -0600

    Update PastViewController.swift
    Generates a pie chart using any stored past budget information. If no information is stored then graph will not be generated.

--- FinanceManagement-App/FinanceManagement-App/PastViewController.swift
+++ FinanceManagement-App/FinanceManagement-App/PastViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Charts
 
-class PastViewController: UIViewController {
+class PastViewController: UIViewController, ChartViewDelegate {
 
     @IBOutlet weak var RentView: UITextField!
     @IBOutlet weak var BillsView: UITextField!
@@ -16,25 +17,64 @@ class PastViewController: UIViewController {
     @IBOutlet weak var SavingsView: UITextField!
     @IBOutlet weak var AddView: UITextField!
 
-    
+    var pieChart = PieChartView()
+        @IBOutlet weak var graphView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         var y = PrevBudgets.shared.Rent
         RentView.text = "\(y)"
+        // if you want to test the pie chart generation uncomment default values for y
+        //y = 25
         y = PrevBudgets.shared.Bills
         BillsView.text = "\(y)"
+        //y = 25
         y = PrevBudgets.shared.Sub
+        //y = 25
         SubView.text = "\(y)"
         y = PrevBudgets.shared.Groceries
+        //y = 25
         GroceriesView.text = "\(y)"
         y = PrevBudgets.shared.Savings
+        //y = 25
         SavingsView.text = "\(y)"
         y = PrevBudgets.shared.Additional
+        //y = 25
         AddView.text = "\(y)"
 
+        pieChart.delegate = self
         // Do any additional setup after loading the view.
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        pieChart.frame = CGRect(x: 0, y: 0,
+                    width: self.view.frame.size.width,
+                    height: self.view.frame.size.width)
+        pieChart.center = graphView.center
+        
+        view.addSubview(pieChart)
+        
+        
+        var entries = [ChartDataEntry]()
+        var y = PrevBudgets.shared.Rent
+        entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
+        y = PrevBudgets.shared.Bills
+        entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
+        y = PrevBudgets.shared.Sub
+            entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
+        y = PrevBudgets.shared.Groceries
+            entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
+        y = PrevBudgets.shared.Savings
+            entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
+        y = PrevBudgets.shared.Additional
+            entries.append(ChartDataEntry(x: Double(y), y: Double(y)))
 
+        let set = PieChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.colorful()
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
+    }
 
     
     /*
